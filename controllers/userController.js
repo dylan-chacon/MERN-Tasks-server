c6eb9bd9def017ea120e77b025c1be9311cb2cf3
Dylan <dachaconp@unal.edu.co>
2020-08-07 11:49:39 -0500

    creando usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,46 @@
-exports.createUser = (req, res) => {
-    console.log(':v')
+const User =  require('../models/User');
+const bcrypt = require('bcrypt')
+const { validationResult } = require('express-validator') 
+const jwt = require('jsonwebtoken');
+
+exports.createUser = async (req, res) => {
+    //extrart email/password
+    const {email, password} = req.body;
+    //errores
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    try {
+        let user = await User.findOne({ email });
+        if(user) {
+            return res.status(400).json({ msg: 'El usuario ya existe' });
+        }
+        //crear usuario
+        user = new User(req.body);
+        //Hash password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        // guardar usuario
+        await user.save();
+        //crear JWT
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        //firmar JWT
+        jwt.sign(payload, process.env.SECRET, {
+            expiresIn: 3600
+        }, (error, token) => {
+            if (error) throw error;
+            //confirmación
+            res.json({token});
+        })
+
+        res.send({ msg: 'Usuario creado correctamente' });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
+const { check } = require('express-validator');
 
 //crear usuario | api/users
 router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email válido').isEmail(),
+        check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 })
+    ],
     userController.createUser
 );
 
