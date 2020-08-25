bb849b31baba948bf941ac3bbb10c50f24f98310
Dylan <dachaconp@unal.edu.co>
2020-08-25 12:19:31 -0500

    deploy changes

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.use(cors());
 //Habilitar express.json
 app.use(express.json({ extended: true }));
 //puerto
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 //importar rutas
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 //arrancar
-app.listen(PORT, () => {
-    console.log(`Servidor funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor funcionando en el puerto ${port}`);
 })
